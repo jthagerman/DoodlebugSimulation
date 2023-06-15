9de21299880ed64f16b7daea5c413c1b30123340
jthagerman <jthagerma@gmail.com>
2023-06-15 17:06:13 -0400

    ignore ts on hook for now

--- Hooks/useGameBoard.tsx
+++ Hooks/useGameBoard.tsx
@@ -1,4 +1,5 @@
 "use client";
+//@ts-nocheck
 import React, { useState, useContext } from "react";
 import { Board } from "@/app/Simulation/board";
 import { BOARD_HEIGHT, BOARD_WIDTH } from "@/app/Simulation/settings";
